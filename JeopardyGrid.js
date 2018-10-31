00d7d2db03dca171b641c45469e2b99ba60cbf05
morthos <pathshan61@gmail.com>
2018-10-31 15:14:56 -0400

    .

--- JeopardyGrid.js
+++ JeopardyGrid.js
@@ -1,48 +1,29 @@
-class JeopardyGrid extends Grid {   
-
-    constructor(options) {
-        options.numberOfColumns = options.categoryIDs.length
-        super(options)
-        this.categoryIDs = options.categoryIDs
-        this.jServiceURL = "http://localhost:3000/api/category/"
-        this.categories = null
-
-        this.getCategories()
-        this.gridContainer.addEventListener("click", this.onClick.bind(this))
+class JeopardyCell extends Cell {
+    constructor (rowIndex, columnIndex, rowParent) {
+        super(rowIndex, columnIndex, rowParent)
     }
 
-    createCell(rowIndex, columnIndex, rowElement) {
-        return new JeopardyCell(rowIndex, columnIndex, this.rowElement)
+    assignCategoryTitle (category) {
+        this.isCategoryHeader = true
+        this.title = category.title
+        this.cell.innerHTML = category.title
     }
 
-    onClick(event) {
-        if (!event.target.classList.contains("cell")) return
-        const cell = event.target.instance
-        if (cell.isCategoryHeader) return
+    assignClue (clues) {
+        const clue = clues[this.rowIndex]
 
-        cell.showQuestion()
+        this.pointValue = this.rowIndex * 100
+        this.question = clue.question
+        this.answer = clue.answer
+        this.category = clue.category
+        this.showPointValue()
     }
 
-    async getCategories() {
-        const categoryPromises = this.categoryIDs.map(id => {
-            return fetch(this.jServiceURL + id).then(res => res.json())
-        })
-
-        this.categories = await Promise.all(categoryPromises)
-        this.assignCluesToCells()
+    showPointValue () {
+        this.cell.innerHTML = "$" + this.pointValue
     }
 
-    assignCluesToCells() {
-        for (let rowIndex = 0; rowIndex < this.numberOfRows; rowIndex++) {
-            for (let columnIndex = 0; columnIndex < this.numberOfColumns; columnIndex++) {
-                const cell = this.gridArray[rowIndex][columnIndex]
-                const category = this.categories[columnIndex]
-                if (rowIndex === 0) {
-                    cell.assignCategoryTitle(category)
-                } else {
-                    cell.assignClue(category.clues)
-                }
-            }
-        }
+    showQuestion () {
+        this.cell.innerHTML = this.question
     }
 }
